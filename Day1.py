c82a2fed010a88a93482c4ffa29bf4e52229b88b
Bibin-2025 <bibinbinu112@gmail.com>
2025-10-20 10:54:48 +0530

    first commit

---
+++ Day1.py
@@ -0,0 +1,17 @@
+applejuice = 15.5
+orangejuice = 20
+grapejuice = 10.25
+
+totalvolume = applejuice + orangejuice + grapejuice
+print(totalvolume)
+
+totalvolume = int(totalvolume)
+print(totalvolume)
+
+totalvolume = str(totalvolume)
+totalvolume = ("the juice is nice")
+print(totalvolume)
+
+import random
+print(random.randrange(5,10))
+
